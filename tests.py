50b71b92fecee36c4fac1724fe3586677b04378f
reinhake <reinhake@oregonstate.edu>
2022-02-07 20:05:55 -0800

    waiting on linting errors

--- tests.py
+++ tests.py
@@ -4,55 +4,48 @@ from credit_card_validator import credit_card_validator
 
 
 class TestCase(unittest.TestCase):
+    # testing visa prefix
     def test1(self):
         for i in range(0,100000):
             val = "4" + str(random.randint(10000000000000, 999999999999999))
             credit_card_validator(val)
 
+    # bug two seems to involve a visa prefix followed by a 0
     def test2(self):
         for i in range(0,100000):
             val = "40" + str(random.randint(10000000000000, 99999999999999))
             credit_card_validator(val)
 
+    # testing first set of mastercard prefixes  
     def test3(self):
         for i in range(0,100000):
             val = "5" + str(random.randint(10000000000000, 999999999999999))
             credit_card_validator(val)
 
+    # testing second set lowerbounds of mastercard prefixes
     def test4(self):
         for i in range(0,50000):
-            val = "22" + str(random.randint(10000000000000, 999999999999999))
+            val = "22" + str(random.randint(10000000000000, 99999999999999))
             credit_card_validator(val)
 
+    # testing second set upperbounds of mastercard prefixes
     def test5(self):
         for i in range(0,50000):
             val = "27" + str(random.randint(10000000000000, 30000000000000))
             credit_card_validator(val)
 
+    # testing AmEx prefixes
     def test6(self):
         for i in range(0,100000):
             val = "3" + str(random.randint(30000000000000, 800000000000000))
             credit_card_validator(val)
 
+    # no specific prefixes just testing any length 14 to 17
     def test7(self):
         for i in range(0,100000):
             val = random.randint(10000000000000, 99999999999999999)
             credit_card_validator(val)
 
-# 4052751135088950 good checksum test 2
 
-# 4528343118254528 bad checksum test 4
-# 7115125492487115 bad checksum test 4
-# 4721512696974721 bad checksum test 4
-# 5294487807775294 good checksum
-# 1212034808503677 good checksum
-
-# 4590829111103396 good checksum test 5
-# 4661311112413367 good checksum test 5
-# 4934511720111151 good
-# 5401811114907425 good
-# 5493811112410124 good
-
-# 342695942809769 good checksum and length test 3
 if __name__ == '__main__':
     unittest.main()
