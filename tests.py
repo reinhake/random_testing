bdd74d88d5ac2d7c417f7910be16714640288bb5
reinhake <reinhake@oregonstate.edu>
2022-02-07 19:18:13 -0800

    test11

--- tests.py
+++ tests.py
@@ -15,24 +15,31 @@ class TestCase(unittest.TestCase):
             credit_card_validator(val)
 
     def test3(self):
-        for i in range(0,100000):
-            val = "2" + str(random.randint(20000000000000, 8000000000000000))
+        for i in range(0,50000):
+            val = "22" + str(random.randint(10000000000000, 999999999999999))
             credit_card_validator(val)
 
     def test4(self):
-        for i in range(0,100000):
-            val = "3" + str(random.randint(30000000000000, 800000000000000))
+        for i in range(0,50000):
+            val = "27" + str(random.randint(10000000000000, 30000000000000))
             credit_card_validator(val)
 
     def test5(self):
         for i in range(0,100000):
+            val = "3" + str(random.randint(30000000000000, 800000000000000))
+            credit_card_validator(val)
+
+    def test6(self):
+        for i in range(0,1000000):
             val = random.randint(10000000000000, 99999999999999999)
             credit_card_validator(val)
+
+
 # 4528343118254528 bad checksum test 4
 # 7115125492487115 bad checksum test 4
 # 4721512696974721 bad checksum test 4
 # 5294487807775294 good checksum
-# 5152069599005152 bad checksum
+# 1212034808503677 good checksum
 
 # 4590829111103396 good checksum test 5
 # 4661311112413367 good checksum test 5
