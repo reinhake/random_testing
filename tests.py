9ace3bcb12f9cafe936f9f03858eb176a5dc9078
reinhake <reinhake@oregonstate.edu>
2022-02-07 19:25:42 -0800

    test13

--- tests.py
+++ tests.py
@@ -5,31 +5,36 @@ from credit_card_validator import credit_card_validator
 
 class TestCase(unittest.TestCase):
     def test1(self):
-        for i in range(0,1000000):
+        for i in range(0,100000):
             val = "4" + str(random.randint(10000000000000, 999999999999999))
             credit_card_validator(val)
 
     def test2(self):
         for i in range(0,100000):
-            val = "5" + str(random.randint(10000000000000, 999999999999999))
+            val = "40" + str(random.randint(10000000000000, 99999999999999))
             credit_card_validator(val)
 
     def test3(self):
-        for i in range(0,50000):
-            val = "22" + str(random.randint(10000000000000, 999999999999999))
+        for i in range(0,100000):
+            val = "5" + str(random.randint(10000000000000, 999999999999999))
             credit_card_validator(val)
 
     def test4(self):
         for i in range(0,50000):
-            val = "27" + str(random.randint(10000000000000, 30000000000000))
+            val = "22" + str(random.randint(10000000000000, 999999999999999))
             credit_card_validator(val)
 
     def test5(self):
+        for i in range(0,50000):
+            val = "27" + str(random.randint(10000000000000, 30000000000000))
+            credit_card_validator(val)
+
+    def test6(self):
         for i in range(0,100000):
             val = "3" + str(random.randint(30000000000000, 800000000000000))
             credit_card_validator(val)
 
-    def test6(self):
+    def test7(self):
         for i in range(0,100000):
             val = random.randint(10000000000000, 99999999999999999)
             credit_card_validator(val)
