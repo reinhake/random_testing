cee70487dbce29571a95068803b4369cbf0cf7d5
reinhake <reinhake@oregonstate.edu>
2022-02-07 18:22:36 -0800

    test1

--- tests.py
+++ tests.py
@@ -5,8 +5,8 @@ from credit_card_validator import credit_card_validator
 
 class TestCase(unittest.TestCase):
     def test1(self):
-        for i in range(0,1000):
-            val = random.randint(1000000000000000, 9999999999999999)
+        for i in range(0,3000):
+            val = random.randint(10000000000000, 99999999999999999)
             credit_card_validator(val)
 
 
