b3ece9139c82bb215fd29e9af619789b87ea91a6
reinhake <reinhake@oregonstate.edu>
2022-02-07 19:07:24 -0800

    test8

--- tests.py
+++ tests.py
@@ -6,17 +6,17 @@ from credit_card_validator import credit_card_validator
 class TestCase(unittest.TestCase):
     def test1(self):
         for i in range(0,10000):
-            val = "4" + str(random.randint(100000000000000, 999999999999999))
+            val = "4" + str(random.randint(10000000000000, 999999999999999))
             credit_card_validator(val)
 
     def test2(self):
         for i in range(0,10000):
-            val = "5" + str(random.randint(100000000000000, 999999999999999))
+            val = "5" + str(random.randint(10000000000000, 999999999999999))
             credit_card_validator(val)
 
     def test3(self):
         for i in range(0,10000):
-            val = "2" + str(random.randint(200000000000000, 8000000000000000))
+            val = "2" + str(random.randint(20000000000000, 8000000000000000))
             credit_card_validator(val)
 
     def test4(self):
@@ -25,15 +25,20 @@ class TestCase(unittest.TestCase):
             credit_card_validator(val)
 
     def test5(self):
-        for i in range(0,1000000):
+        for i in range(0,100000):
             val = random.randint(1000000000, 99999999999999999999)
             credit_card_validator(val)
 # 4528343118254528 bad checksum test 4
 # 7115125492487115 bad checksum test 4
 # 4721512696974721 bad checksum test 4
+# 5294487807775294 good checksum
+# 5152069599005152 bad checksum
 
 # 4590829111103396 good checksum test 5
 # 4661311112413367 good checksum test 5
+# 4934511720111151 good
+# 5401811114907425 good
+# 5493811112410124 good
 
 # 342695942809769 good checksum and length test 3
 if __name__ == '__main__':
